bd7e2ab57522fab7425e0a14d80358078d3c059e
ourenboku1 <143403593+ourenboku1@users.noreply.github.com>
2023-09-06 19:23:33 +0800

    Add files via upload

--- line_bot_api.py
+++ line_bot_api.py
@@ -5,7 +5,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent, StickerSendMessage, ImageSendMessage, LocationSendMessage,ImageCarouselTemplate,ImageCarouselColumn, PostbackAction, TemplateSendMessage, FlexSendMessage ,ButtonsTemplate ,PostbackEvent ,QuickReplyButton,QuickReply , ConfirmTemplate ,MessageAction
+    MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent, StickerSendMessage, ImageSendMessage, LocationSendMessage,ImageCarouselTemplate,ImageCarouselColumn, PostbackAction, TemplateSendMessage, FlexSendMessage ,ButtonsTemplate ,PostbackEvent ,QuickReplyButton,QuickReply , ConfirmTemplate ,MessageAction, Reservation
 )
 
 line_bot_api = LineBotApi('Sz1hgt3TkPaey3FgvI5zAxVhJTcy5aBztZSKGJwrx1hN1/3j8UtlfkRzoEGybPNwFlaChLfdBpHLyp0LI3GUt/P5SHMEHQ+TpnsrVpYeRLgqfM2LN8PhtML/1Z2LaP+gmcB6MAAOu6tjLXhcFzn0twdB04t89/1O/w1cDnyilFU=')
